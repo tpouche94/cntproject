13fdd47d7495a35b66d67221d161272b8a59dc10
Jordan <jordanjoseph94@yahoo.com>
2020-07-19 13:54:55 -0400

    static feed app done

---
+++ static_feed/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+from embed_video.fields import EmbedVideoField
+
+class Item(models.Model):
+    video = EmbedVideoField()  # same like models.URLField()
